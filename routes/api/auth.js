e386f5df2fbb3f7bbebae95c57f5e7a6c1d54185
Siviya Stoyanova <silviya.stoyanova@siili.com>
2022-08-17 19:38:51 +0300

    Get user data correctly

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -13,8 +13,8 @@ router.get("/", authMiddleware, async (req, res) => {
   // get the user data
 
   try {
-    const user = await User.findById(req.user.id).select("-password");
-    res.json(user);
+    const user = await User.findById(req.user._id).select("-password");
+    res.json({user});
   } catch ({ message }) {
     res.status(500).json({ message });
   }
